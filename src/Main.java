3557aba3d65b8c5f866bb9ecdf079729b258b0fb
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-11 16:18:15 +0200

    Arrays fill logic done

--- src/Main.java
+++ src/Main.java
@@ -26,26 +26,50 @@ public class Main {
             String[] numberString1 = numberString.split(" ");
 
             // Проверка:  если пользователь ввел меньшее количество аргументов с консоли
-            // чем длина массива, то массив заполнится только введенными аргументами
+            // чем длина массива, то массив заполнится только введенными данными
             // остальные значения будут по умолчанию равны == 0
 
-            if (numberString1.length < sizeArray) sizeArray = numberString1.length;
+            if (numberString1.length <= sizeArray) {
 
-            // Преобразование строчного массива в Int, и копирование результата в int массив array
+                sizeArray = numberString1.length;   //  эта строчка нужна если (numberString1.length < sizeArray)
 
-                for(int i = 0; i < sizeArray; i++) {                  //  sizeArray, а не numberString1.length
-                    array[i] = Integer.parseInt(numberString1[i]);    //  чтобы не было выхода за рамки массива
+                // Преобразование строчного массива в Int, и копирование результата в int массив array
+
+                for (int i = 0; i < sizeArray; i++) {                  //  sizeArray, а не numberString1.length
+                    array[i] = Integer.parseInt(numberString1[i]);     //  чтобы не было выхода за рамки массива
+                }                                                      //  если пользователь введет больше аргументов
+
+                doingArray(array);
+            }
+
+            /*  Пользователь может ввести и бОльшее количество аргументов, чем он вначале заявил, задавая
+            //  длину массива. Тогда (если сейчас оставить код как есть) будет учитываться ровно столько аргументов,
+            //  сколько соответствует изначально заданному пользователем размеру массива, а все лишние будут отброшены
+
+            //  Но можно и усложнить вариант:
+            //  в случае, если пользователь ввел бОльшее количество аргументов, чем он вначале явно заявил,
+            //  тогда нужно заново инициализировать массив  */
+
+            if (numberString1.length > sizeArray) {
+                sizeArray = numberString1.length;
+                int[] array2 = new int[sizeArray];
+
+                for (int i = 0; i < sizeArray; i++) {                    //  sizeArray здесь равен именно реальной
+                    array2[i] = Integer.parseInt(numberString1[i]);      //  длине массива (numberString1.length);
                 }
 
+                doingArray(array2);    //  Здесь передается аргументом именно новый массив array2
+                                       //  чей размер соответствует реально введенному пользователем
+                                       //  количеству цифр с консоли
+            }
 
-            doingArray(array);
 
         }
 
         else {
             // COMPLICATED variant
-            // пользователь просто вводит в массив данные в любом количестве
-            // а массив создается нужного размера
+            // пользователь просто вводит в массив данные в любом количестве аргументов
+            // а массив создается сразу соответствующего размера
 
             if (variant == 2) {
 
@@ -56,7 +80,7 @@ public class Main {
                 String numberString = InputData.stringInput();
                 String[] numberString1 = numberString.split(" ");
 
-            // Создание массива нужного размера
+            // Создание массива требуемого размера
                 int sizeArray = (numberString1.length);
                 int[] array = new int[sizeArray];
 
@@ -66,17 +90,6 @@ public class Main {
                 for(int i = 0; i < sizeArray; i++) {                 // sizeArray здесь равен numberString1.length
                     array[i] = Integer.parseInt(numberString1[i]);
                 }
-
-                /*
-                // Вывод массива на экран (нужно только для фазы тестирования)
-                System.out.println("\nARRAY  :   ");
-                do
-                {
-                    System.out.println(array[sizeArray-1]);
-                    sizeArray--;
-                }
-                while (sizeArray > 0);
-                */
 
 
              doingArray(array);
