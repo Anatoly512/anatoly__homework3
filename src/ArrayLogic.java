f412d88acc5d9d02660d69ae40b97e3aff287c9f
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-13 14:00:18 +0200

    add some comments

--- src/ArrayLogic.java
+++ src/ArrayLogic.java
@@ -3,9 +3,13 @@ import java.util.Arrays;
 public class ArrayLogic {
 
     public int[] MySortArray (int ...array) {
+        //  Поиск максимального\минимального числа в массиве
+        //  методом собственной сортировки массива (слева направо, по убыванию)
 
         int[] array2 = new int[array.length];  // именно в новый массив array2 будет записываться результат
         array2 = Arrays.copyOf(array, array.length);  // это нужно только для фазы тестирования, ПОТОМ УДАЛИТЬ!
+
+
 
 
         return array2;
